ebe4052c598713e32bff9728b383e0ec06c8e9c8
Devdatt Vajifdar <70492585+vajifdardevdatt@users.noreply.github.com>
2021-08-19 19:46:21 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html  lang="en" >
+<meta charset="UTF-8"  method="GET">
+<head>
+
+<title>'Devdatt Vajifdar PHP'</title>
+</head>
+
+<body>
+<h1>Devdatt Vajifdar PHP</h1>
+<p>The SHA256 hash of "Devdatt Vajifdar" is
+6ECC5CD95BA393E2851D26103F27589AC5B51E2396897AE96D8C37FB1BD6E336</p>
+<pre>ASCII art of the first letter :
+
+    .-..-.
+    | | ~.-.
+    | |  | |
+    | |  | |
+    | | _`-'
+     `-'`-'
+    
+
+
+
+
+
+
+</body>
+</html>
---
+++ new.php
@@ -0,0 +1,31 @@
+
+
+
+
+<!DOCTYPE html>
+<head><title>Devdatt Vajifdar PHP</title></head>
+<body style="font-family: sans-serif">
+<h1>MD5 cracker</h1>
+<p>This application takes an MD5 hash
+of a four digit pin and check all 10,000
+possible four digit PINs to determine the PIN.</p>
+<pre>Debug Output:
+</pre>
+<p>PIN: Not found</p>
+<form>
+<input type="text" name="md5" size="40"
+  value=""/>
+<input type="submit" value="Crack MD5"/>
+</form>
+<ul>
+<li><a href="new.php">Reset this page</a></li>
+<li><a href="makepin.php">Make an MD5 PIN</a></li>
+<li><a href="md5.php">MD5 Encoder</a></li>
+<li><a href="https://www.wa4e.com/assn/crack/" target="_blank">Specification
+for this assignment</li>
+<li><a
+href="https://github.com/csev/php-intro/tree/master/code/crack"
+target="_blank">Source code similar to this application</a></li>
+</ul>
+</body>
+</html>
